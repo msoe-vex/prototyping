a49fb0beb3292365fd32b5d9be5bbfec6862f91c
Dylan Powers <powersd@msoe.edu>
2022-09-23 19:09:26 -0500

    setup project

--- src/main.cpp
+++ src/main.cpp
@@ -79,14 +79,11 @@ void opcontrol() {
 	pros::Motor right_mtr(2);
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
-
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+		
 	}
 }
+
+// 2 3-motor-group controlled by toggle (same ports)
+// 2 3-motor-groups controlled by a joystick (same ports)
+// 1 misc port controller by joystick
+// 1 misc port controller by button
